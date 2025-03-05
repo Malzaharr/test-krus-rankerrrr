dade90a0b3ecc44aa8cdfb92148189052b77bce0
Malzaharr <mahajan2005@mail.ru>
2025-03-05 05:11:03 +0300

    not_xd(

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
-const port = 3307;
+const port = 3306;
 
 app.use(cors({
     origin: ['https://spiffy-peony-6368f0.netlify.app','http://localhost:3000','http://37.212.31.223:3307'], // Замените на свой домен Netlify
@@ -13,7 +13,7 @@ app.use(cors({
 function createConnection() {
     const db = mysql.createConnection({
         host: 'localhost',
-        port: 3307,// Значение по умолчанию для локальной разработки
+        port: 3306,// Значение по умолчанию для локальной разработки
         user: 'root',      // Значение по умолчанию для локальной разработки
         password: '',  // Значение по умолчанию для локальной разработки
         database: 'diplom' // Значение по умолчанию для локальной разработки
